04714f3e4331f3eb59ebcb0c89822cab2bd7398e
ownl3fans <codevoicex@gmail.com>
2026-05-25 23:03:51 +0300

    Update bot/main.py

--- bot/main.py
+++ bot/main.py
@@ -98,5 +98,8 @@ async def main() -> None:
 if __name__ == "__main__":
     try:
         asyncio.run(main())
-    except KeyboardInterrupt:
-        sys.exit(0)
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        print(f"FATAL ERROR: {e}")
+        sys.exit(1)
